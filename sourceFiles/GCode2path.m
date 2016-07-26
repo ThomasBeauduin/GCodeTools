f7d6115c0df6aa77fbcd6333f0774c4e06e98a34
Thomas Beauduin <thomasbeauduin@gmail.com>
2016-07-26 13:28:54 +0900

    Extension added for angle
    extension for angle between 2 gcode commands

--- sourceFiles/GCode2path.m
+++ sourceFiles/GCode2path.m
@@ -1,4 +1,4 @@
-function [path] = GCode2path(input,ds,startPos)
+function [path,theta] = GCode2path(input,ds,startPos)
 %GCODE2PATH - convert G-Code to XYZ coordinates
 %
 % input     : string of G-code text file
@@ -14,13 +14,26 @@ path = [];
 numCommands = numel(gcode);
 for j = 1:numCommands
     % convert string into array of command values
-    command = readGCode(char(gcode(j)));
+    cmd(j,:) = readGCode(char(gcode(j)));
     
     % interpolate GCode to path with distance ds
-    [array, startPos] = interpGCode(command, startPos, ds);
+    [array, startPos] = interpGCode(cmd(j,:), startPos, ds);
  
     % add set of points to existing path
     path = [path; array];
+end
+
+theta = [];
+for j = 1:numCommands-1
+    if cmd(j,1)==1 && cmd(j+1,1)==1 
+        S1 = sqrt(cmd(j,2)^2 + cmd(j,3)^2 + cmd(j,4)^2);
+        S2 = sqrt(cmd(j+1,2)^2+cmd(j+1,3)^2 + cmd(j+1,4)^2);
+        S3 = sqrt((cmd(j,2)+cmd(j+1,2))^2 + ...
+                (cmd(j,3)+cmd(j+1,3))^2+(cmd(j,4)+cmd(j+1,4))^2);
+        theta(j) = acos( (S3^2-S1^2-S2^2)/(-2*S1*S2) )*180/pi;
+    else
+        % G3/G2 calculation
+        theta(j) = 90;
 end
 
 end
